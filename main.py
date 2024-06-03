f195128f152132714ecf65e3f056f8d23842be32
2bfree <2bfree@gmail.com>
2024-06-03 13:16:13 +0900

    main

--- main.py
+++ main.py
@@ -19,17 +19,29 @@ if api_key:
 # File uploader
 audio_file = st.file_uploader(label="Upload audio file", 
                          type=['wav','mp3','m4a'])
+
+
+def transcribe_audio(audio_file):
+    transcription = openai.Audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        language='ko'
+    )
+    return transcription['text']
+
+
 if audio_file is not None:
       # response = openai.Audio.transcribe(model="whisper-1", 
       #                                    file=audio, 
       #                                    api_key=api_key)
-      response = openai.audio.transcriptions.create(model="whisper-1", 
-                                         file=audio_file,
-                                         language='ko')
+      # response = openai.audio.transcriptions.create(model="whisper-1", 
+      #                                    file=audio_file,
+      #                                    language='ko')
    # Split the text into chunks
-   
-      transcriptions = response["text"]
-      docs = split_text(text=transcriptions, chunk_size=1000, chunk_overlap=100)
+  
+      transcript = transcribe_audio(audio_file)
+      # transcriptions = response["text"]
+      docs = split_text(text=transcript, chunk_size=1000, chunk_overlap=100)
       #initialize the LLM
       llm = initialize_llm(model="gpt-4o", temperature=0.1, api_key=api_key)
       #llm2 = initialize_openai(model="gpt-3.5-turbo-16k", temperature=0, api_key=api_key)
