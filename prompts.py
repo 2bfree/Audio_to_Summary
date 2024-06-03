9a04754a5880d701ddf1b4c95f60fa0d540e8f89
2bfree <2bfree@gmail.com>
2024-06-03 14:25:00 +0900

    main

--- prompts.py
+++ prompts.py
@@ -24,17 +24,31 @@ prompt_template = PromptTemplate(
     input_variables=["text"],
 )
 
+# refine_template = (
+#     "Your job is to produce a final summary\n"
+#     "We have provided an existing summary up to a certain point: {existing_answer}\n"
+#     "We have the opportunity to refine the existing summary"
+#     "(only if needed) with some more context below.  \n"
+#     "------------\n"
+#     "{text}\n"
+#     "------------\n"
+#     "Given the new context, refine the original summary within a meeting report form and"
+#     "summary some keypoints from original text in korean."
+#     )
+
 refine_template = (
-    "Your job is to produce a final summary\n"
-    "We have provided an existing summary up to a certain point: {existing_answer}\n"
-    "We have the opportunity to refine the existing summary"
-    "(only if needed) with some more context below.  \n"
+    "너는 회사 기획실 실무자이고 너의 역할은 임원 회의의 회의록을 작성하는 것이야.\n"
+    "1차 정리한 내용은 다음과 같아: {existing_answer}\n"
+    "더 많은 컨텍스트를 통해 기존 요약을 정리할 수 있어. 단 1000자 이내에서 정리해줘."
+    "(필요한 경우에만) 아래에 더 많은 컨텍스트가 제공됩니다.\n"
     "------------\n"
     "{text}\n"
     "------------\n"
-    "Given the new context, refine the original summary within a meeting report form and"
-    "summary some keypoints from original text in korean."
+    "전체적인 컨텍스트를 고려해서 회의록 형식의 보고서를 작성해줘."
+    "원래 텍스트에서 핵심 포인트는 별도로 요약 정리해줘."
     )
+
+
 refine_prompt = PromptTemplate(
     template=refine_template,
     input_variables=["existing_answer", "text"],
